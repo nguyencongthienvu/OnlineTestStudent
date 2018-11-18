850da12a246f725aaecae0afb64224f79853e60f
thienvunguyen <nguyencongthienvu@gmail.com>
2018-11-18 22:29:00 +0700

    Change css + change link + text html.

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileServiceService } from '../../services/profile-service.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  Profile:any=[];
+  constructor(private profile:ProfileServiceService) { }
 
   ngOnInit() {
+    const token = localStorage.getItem('token');
+    if(token != '' || token != null ) 
+    {
+      if (token ) {
+        this.profile.Profile(token).then((profiledata) => {
+          
+          if (profiledata.json().status === 'successfully') {
+            this.Profile = profiledata.json().data[0]     
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      }
+    }
   }
 
 }
